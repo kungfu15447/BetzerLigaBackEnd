95a8acdd7905e19028bfc93aa9d3ae78845bdc84
milimolo <bondemand@hotmail.dk>
2019-11-26 10:44:21 +0100

    IUserService

--- BetzerLiga/ClassLibrary2/ApplicationService/IUserService.cs
+++ BetzerLiga/ClassLibrary2/ApplicationService/IUserService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BetzerLiga.Core.Entity;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,17 @@ namespace BetzerLiga.Core.ApplicationService
 {
     public interface IUserService
     {
+        IEnumerable<User> GetAll();
 
+        User GetUserById(int id);
+
+        void Add(User user);
+
+        User Update(User UserToUpdate);
+
+        User Delete(int id);
+
+        User NewUser(string firstName, string lastName, bool isAdmin, string email,
+            byte[] passwordHash, byte[] passwordSalt, List<UserTour> tournaments, List<UserMatch> tips, List<User> following);
     }
 }
---
+++ BetzerLiga/ClassLibrary2/ApplicationService/Implementation/UserService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BetzerLiga.Core.ApplicationService.Implementation
+{
+    public class UserService
+    {
+
+    }
+}
