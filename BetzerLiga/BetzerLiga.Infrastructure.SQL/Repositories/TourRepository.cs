49f0cf7bd6c2a69431b6222443920d2af007aa6d
Frederik Jensen <kungfu15447@users.noreply.github.com>
2019-11-26 12:25:50 +0100

    Implementeret metoder i TourRepository

--- BetzerLiga/BetzerLiga.Infrastructure.SQL/Repositories/TourRepository.cs
+++ BetzerLiga/BetzerLiga.Infrastructure.SQL/Repositories/TourRepository.cs
@@ -1,36 +1,50 @@
 ﻿using BetzerLiga.Core.DomainService;
 using BetzerLiga.Core.Entity;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace BetzerLiga.Infrastructure.SQL.Repositories
 {
     public class TourRepository : ITourRepository
     {
+        private BetzerLigaContext _context;
+
+        public TourRepository(BetzerLigaContext context)
+        {
+            _context = context;
+        }
         public Tournament CreateTour(Tournament Tour)
         {
-            throw new NotImplementedException();
+            _context.Attach(Tour).State = EntityState.Added;
+            _context.SaveChanges();
+            return Tour;
         }
 
         public Tournament DeleteTour(Tournament Tour)
         {
-            throw new NotImplementedException();
+            _context.Remove(Tour);
+            _context.SaveChanges();
+            return Tour;
         }
 
         public IEnumerable<Tournament> ReadAll()
         {
-            throw new NotImplementedException();
+            return _context.Tournaments;
         }
 
         public Tournament ReadTourById(int id)
         {
-            throw new NotImplementedException();
+            return _context.Tournaments
+                .FirstOrDefault(t => t.Id == id);
         }
 
         public Tournament UpdateTour(Tournament Tour)
         {
-            throw new NotImplementedException();
+            _context.Attach(Tour).State = EntityState.Modified;
+            return Tour;
         }
     }
 }
