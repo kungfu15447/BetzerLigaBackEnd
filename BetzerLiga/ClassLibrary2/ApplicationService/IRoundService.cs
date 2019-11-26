aebc98ae5990d3f9f5529e429b925761cdcfa6b9
kristianUrup <43743914+kristianUrup@users.noreply.github.com>
2019-11-26 11:00:59 +0100

    Interfaces and classes are made

---
+++ BetzerLiga/ClassLibrary2/ApplicationService/IRoundService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BetzerLiga.Core.Entity;
+
+namespace BetzerLiga.Core.ApplicationService
+{
+    public interface IRoundService
+    {
+        Round Create(Round round);
+        Round ReadById(int id);
+        List<Round> ReadAll();
+        Round Update(Round roundUpdated);
+        Round Delete(int id);
+    }
+}
---
+++ BetzerLiga/ClassLibrary2/ApplicationService/Implementation/RoundService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BetzerLiga.Core.Entity;
+
+namespace BetzerLiga.Core.ApplicationService.Implementation
+{
+    public class RoundService:IRoundService
+    {
+        public Round Create(Round round)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Round ReadById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Round> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Round Update(Round roundUpdated)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Round Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
