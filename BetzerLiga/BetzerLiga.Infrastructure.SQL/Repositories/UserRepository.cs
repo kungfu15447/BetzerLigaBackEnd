78e4309ec13dff52d32f2a0ddffb488a4acaab58
milimolo <bondemand@hotmail.dk>
2019-11-26 12:25:08 +0100

    add user

--- BetzerLiga/BetzerLiga.Infrastructure.SQL/Repositories/UserRepository.cs
+++ BetzerLiga/BetzerLiga.Infrastructure.SQL/Repositories/UserRepository.cs
@@ -1,10 +1,47 @@
-﻿using System;
+﻿using BetzerLiga.Core.DomainService;
+using BetzerLiga.Core.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BetzerLiga.Infrastructure.SQL.Repositories
 {
-    class UserRepository
+    class UserRepository : IUserRepository
     {
+        private BetzerLigaContext _context;
+
+        public UserRepository(BetzerLigaContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(User user)
+        {
+            if(user != null)
+            {
+                _context.Attach(user).State = EntityState.Added;
+            }
+        }
+
+        public User Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Update(User UserToUpdate)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
