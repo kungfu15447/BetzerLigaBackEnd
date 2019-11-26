d361206f92edc7b8565b1aca79eeefa9af868c85
Schweizeren <j_wannerhenriksen@hotmail.com>
2019-11-26 10:59:18 +0100

    TestDriven

---
+++ BetzerLiga/BetzerLiga.Infrastructure.SQL/Repositories/AuthenticationHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BetzerLiga.Core.Entity;
+
+namespace BetzerLiga.Core.ApplicationService.Authentication
+{
+    public class AuthenticationHelper : IAuthenticationHelper
+    {
+        private byte[] secretBytes;
+
+        public AuthenticationHelper(Byte[] secret)
+        {
+            secretBytes = secret;
+        }
+
+        public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GenerateToken(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
