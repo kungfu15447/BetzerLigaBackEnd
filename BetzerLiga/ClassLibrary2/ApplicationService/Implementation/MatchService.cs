dab4b5e3daf104d6588a30540de03417504ef0cc
Frederik Jensen <kungfu15447@users.noreply.github.com>
2019-11-27 12:03:01 +0100

    Færdiggjort matchservice

--- BetzerLiga/ClassLibrary2/ApplicationService/Implementation/MatchService.cs
+++ BetzerLiga/ClassLibrary2/ApplicationService/Implementation/MatchService.cs
@@ -1,10 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using BetzerLiga.Core.DomainService;
+using BetzerLiga.Core.Entity;
 
 namespace BetzerLiga.Core.ApplicationService.Implementation
 {
-    public class MatchService
+    public class MatchService : IMatchService
     {
+        private IMatchRepository _matchRepo;
+        public MatchService(IMatchRepository matchRepo)
+        {
+            _matchRepo = matchRepo;
+        }
+        public Match CreateMatch(Match match)
+        {
+            return _matchRepo.CreateMatch(match);
+        }
+
+        public Match DeleteMatch(Match match)
+        {
+            return _matchRepo.DeleteMatch(match);
+        }
+
+        public List<Match> GetAllMatches()
+        {
+            return _matchRepo.ReadAll().ToList();
+        }
+
+        public Match GetMatchById(int id)
+        {
+            return _matchRepo.ReadMatchById(id);
+        }
+
+        public Match UpdateMatch(Match match)
+        {
+            return _matchRepo.UpdateMatch(match);
+        }
     }
 }
