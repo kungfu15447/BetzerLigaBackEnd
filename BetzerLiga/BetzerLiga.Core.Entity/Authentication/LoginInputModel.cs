48bf19587e650ef1d1c5e7de863b6394e8d49478
Schweizeren <j_wannerhenriksen@hotmail.com>
2019-11-27 12:45:35 +0100

    Authentication

---
+++ BetzerLiga/BetzerLiga.Core.Entity/Authentication/LoginInputModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BetzerLiga.Core.Entity.Authentication
+{
+    public class LoginInputModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ BetzerLiga/BetzerLiga.RestAPI/Initializer/DBInitializer.cs
@@ -0,0 +1,38 @@
+﻿using BetzerLiga.Core.DomainService;
+using BetzerLiga.Core.Entity;
+using BetzerLiga.Infrastructure.SQL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BetzerLiga.RestAPI.Initializer
+{
+    public class DBInitializer
+    {
+        private IAuthenticationHelper authenticationHelper;
+        public DBInitializer(IAuthenticationHelper authHelper)
+        {
+            authenticationHelper = authHelper;
+        }
+
+        public void Seed(BetzerLigaContext ctx)
+        {
+            string password = "1234";
+            Byte[] passwordHashUser, passwordSaltUser;
+
+            authenticationHelper.CreatePasswordHash(password, out passwordHashUser, out passwordSaltUser);
+
+            User user1 = new User
+            {
+                Firstname = "Mads",
+                Lastname = "Madsen",
+                Email = "MadsMadsen@Mads.dk",
+                PasswordHash = passwordHashUser,
+                PasswordSalt = passwordSaltUser,
+                IsAdmin = false
+            };
+
+        }
+    }
+}
