9b147a43cf29468e4abdb5416db80ba29edf7a1a
kristianUrup <43743914+kristianUrup@users.noreply.github.com>
2019-11-27 13:11:17 +0100

    rest api made

--- BetzerLiga/BetzerLiga.RestAPI/Startup.cs
+++ BetzerLiga/BetzerLiga.RestAPI/Startup.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BetzerLiga.Core.ApplicationService;
+using BetzerLiga.Core.ApplicationService.Implementation;
+using BetzerLiga.Core.DomainService;
+using BetzerLiga.Infrastructure.SQL.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +29,9 @@ namespace BetzerLiga.RestAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IRoundRepository, RoundRepository>();
+            services.AddScoped<IRoundService, RoundService>();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
