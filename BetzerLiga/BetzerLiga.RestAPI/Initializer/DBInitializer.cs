9197ef0969bd497305b081e064163e276993993d
Schweizeren <j_wannerhenriksen@hotmail.com>
2019-11-28 12:21:12 +0100

    Authentication + DBInitializer

--- BetzerLiga/BetzerLiga.RestAPI/Initializer/DBInitializer.cs
+++ BetzerLiga/BetzerLiga.RestAPI/Initializer/DBInitializer.cs
@@ -27,12 +27,71 @@ namespace BetzerLiga.RestAPI.Initializer
             {
                 Firstname = "Mads",
                 Lastname = "Madsen",
-                Email = "MadsMadsen@Mads.dk",
+                Email = "MadsMadsen@google.dk",
                 PasswordHash = passwordHashUser,
                 PasswordSalt = passwordSaltUser,
-                IsAdmin = false
+                IsAdmin = true,
+                
             };
+
+            User user2 = new User
+            {
+                Firstname = "Lars",
+                Lastname = "Lars",
+                Email = "LarsLarsen@google.dk",
+                PasswordHash = passwordHashUser,
+                PasswordSalt = passwordSaltUser,
+                IsAdmin = true
+            };
+
+            User user3 = new User
+            {
+                Firstname = "Frederik",
+                Lastname = "Frederiksen",
+                Email = "FrederikFrederiksen@google.dk",
+                PasswordHash = passwordHashUser,
+                PasswordSalt = passwordSaltUser,
+                IsAdmin = true
+            };
+
+            User user4 = new User
+            {
+                Firstname = "Ole",
+                Lastname = "Olsen",
+                Email = "Olsen@Mads.dk",
+                PasswordHash = passwordHashUser,
+                PasswordSalt = passwordSaltUser,
+                IsAdmin = true
+            };
+
+            User user5 = new User
+            {
+                Firstname = "John",
+                Lastname = "Johnsen",
+                Email = "JohnJohnsen@google.dk",
+                PasswordHash = passwordHashUser,
+                PasswordSalt = passwordSaltUser,
+                IsAdmin = true
+            };
+
+            User user6 = new User
+            {
+                Firstname = "Erik",
+                Lastname = "Eriksen",
+                Email = "ErikEriksen@google.dk",
+                PasswordHash = passwordHashUser,
+                PasswordSalt = passwordSaltUser,
+                IsAdmin = true
+            };
+
+
+
             user1 = ctx.Users.Add(user1).Entity;
+            user2 = ctx.Users.Add(user2).Entity;
+            user3 = ctx.Users.Add(user3).Entity;
+            user4 = ctx.Users.Add(user4).Entity;
+            user5 = ctx.Users.Add(user5).Entity;
+            user6 = ctx.Users.Add(user6).Entity;
 
             ctx.SaveChanges();
 
