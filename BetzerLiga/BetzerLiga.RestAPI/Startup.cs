589a5ac0a116585a3555d950e3a0fcf5be1f051a
milimolo <bondemand@hotmail.dk>
2019-11-28 11:20:59 +0100

    cors

--- BetzerLiga/BetzerLiga.RestAPI/Startup.cs
+++ BetzerLiga/BetzerLiga.RestAPI/Startup.cs
@@ -92,11 +92,20 @@ namespace BetzerLiga.RestAPI
                 opt.UseSqlServer(Configuration.GetConnectionString
                 ("defaultConnection")));
             }
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin",
+                    builder => builder
+                        .AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
+                    );
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors("AllowSpecificOrigin");
+
             if (env.IsDevelopment())
             {
                 using (var scope = app.ApplicationServices.CreateScope())
