381c83e182d5e8e60672355142043436c8f1f7d8
Frederik Jensen <kungfu15447@users.noreply.github.com>
2019-12-18 15:28:06 +0100

    Slettet noget ugyldigt kode

--- BetzerLiga/BetzerLiga.Infrastructure.SQL/Repositories/TourRepository.cs
+++ BetzerLiga/BetzerLiga.Infrastructure.SQL/Repositories/TourRepository.cs
@@ -103,69 +103,6 @@ namespace BetzerLiga.Infrastructure.SQL.Repositories
                         }).ToList()
                 })
                 .FirstOrDefault(t => t.Id == Id);
-
-
-            /*
-                .Include(r => r.Rounds.Select(ro => new Round
-                {
-                    Id = ro.Id,
-                    RoundNumber = ro.RoundNumber,
-                    TournamentId = ro.TournamentId,
-                    TotalGoals = ro.TotalGoals,
-                    TippingDeadLine = ro.TippingDeadLine,
-                    RoundPoints = ro.RoundPoints
-                }))
-                .ThenInclude(r => r.RoundPoints/*
-                .Select(ur => new UserRound 
-                {
-                    Id = ur.Id,
-                    UserId = ur.UserId,
-                    RoundId = ur.RoundId,
-                    UserPoints = ur.UserPoints
-                }))
-                .Include(r => r.Rounds/*.Select(ro => new Round
-                {
-                    Id = ro.Id,
-                    RoundNumber = ro.RoundNumber,
-                    TournamentId = ro.TournamentId,
-                    TotalGoals = ro.TotalGoals,
-                    TippingDeadLine = ro.TippingDeadLine,
-                    Matches = ro.Matches
-                }))
-                .ThenInclude(r => r.Matches
-                /*.Select(m => new Match 
-                {
-                    Id = m.Id,
-                    HomeTeam = m.HomeTeam,
-                    HomeScore = m.HomeScore,
-                    GuestTeam = m.GuestTeam,
-                    GuestScore = m.GuestScore,
-                    StartDate = m.StartDate,
-                    RoundId = m.RoundId,
-                    Tips = m.Tips
-                }))
-                .ThenInclude(m => m.Tips
-                /*.Select(um => new UserMatch 
-                {
-                    Id = um.Id,
-                    UserId = um.UserId,
-                    MatchId = um.MatchId,
-                    Rating = um.Rating,
-                    HomeTip = um.HomeTip,
-                    GuestTip = um.GuestTip,
-                    TotalPoints = um.TotalPoints
-                }))
-                .Include(t => t.Participants
-                /*.Select(ut => new UserTour 
-                {
-                    Id = ut.Id,
-                    User = ut.User,
-                    UserId = ut.UserId,
-                    TournamentId = ut.TournamentId,
-                    TotalUserPoints = ut.TotalUserPoints
-                }))
-                .ThenInclude(p => p.User)
-                .FirstOrDefault(t => t.Id == Id);*/
         }
 
         public Tournament UpdateTour(Tournament Tour)
